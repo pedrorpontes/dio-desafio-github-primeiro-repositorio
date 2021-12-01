e9fcee7690737b2bca730b07185ba106410e7f41
pedrorpontes <pedrorpontes@gmail.com>
2021-12-01 17:58:50 -0300

    Criacao dos projetos utilizando o .NET5 e .NET6

---
+++ DigitalInnovationOne5.0/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dio_desafio_github_primeiro_repositorio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
