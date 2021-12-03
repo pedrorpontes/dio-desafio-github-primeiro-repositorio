f42214df6bf9f5cf74e701ae7d97bc2c5460b7f0
pedrorpontes <pedrorpontes@gmail.com>
2021-12-03 01:00:52 -0300

    Adicionada pasta Revisao

---
+++ Revisao/Revisao/Revisao/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+
+namespace Revisao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno[] alunos = new Aluno[5];
+            int indiceAluno = 0;
+
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        Console.WriteLine("Informe o nome do aluno:");
+                        Aluno aluno = new Aluno();
+                        aluno.Nome = Console.ReadLine();
+
+                        Console.WriteLine("Informe a nota do aluno:");
+
+                        if (decimal.TryParse(Console.ReadLine(), out decimal result))
+                        {
+                            aluno.Nota = result;
+                        }
+                        else
+                        {
+                            throw new ArgumentException("Valor da nota deve ser decimal");
+                        }
+
+                        alunos[indiceAluno] = aluno;
+                        indiceAluno++;
+
+                        break;
+
+                    case "2":
+                        foreach (Aluno a in alunos)
+                        {
+                            if (!string.IsNullOrEmpty(a.Nome))
+                            {
+                                Console.WriteLine($"Aluno: {a.Nome} - Nota: {a.Nota}");
+                            }
+                        }
+                        break;
+
+                    case "3":
+                        decimal notaTotal = 0;
+                        var nrAlunos = 0;
+                        for (int i = 0; i < alunos.Length; i++)
+                        {
+                            if (!string.IsNullOrEmpty(alunos[i].Nome))
+                            {
+                                notaTotal += alunos[i].Nota;
+                                nrAlunos++;
+                            }
+
+                        }
+                        var media = notaTotal / nrAlunos;
+                        ConceitoEnum conceitoGeral;
+
+                        if (media < 2)
+                        {
+                            conceitoGeral = ConceitoEnum.E;
+                        }
+                        else if (media < 4)
+                        {
+                            conceitoGeral = ConceitoEnum.D;
+                        }
+                        else if (media < 6)
+                        {
+                            conceitoGeral = ConceitoEnum.C;
+                        }
+                        else if (media < 8)
+                        {
+                            conceitoGeral = ConceitoEnum.B;
+                        }
+                        else
+                        {
+                            conceitoGeral = ConceitoEnum.A;
+                        }
+
+
+                        Console.WriteLine($"Média Geral: {media} - Conceito:{conceitoGeral}" );
+
+                        break;
+
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                opcaoUsuario = ObterOpcaoUsuario();
+            }
+
+        }
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Informe a opção desejada:");
+            Console.WriteLine();
+            Console.WriteLine("1- Inserir novo aluno");
+            Console.WriteLine("2- Listar alunos");
+            Console.WriteLine("3- Calcular média geral");
+            Console.WriteLine("X- Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine();
+            Console.WriteLine();
+            return opcaoUsuario;
+        }
+    }
+
+
+}
